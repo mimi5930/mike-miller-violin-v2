927e8a3e1235c2a1c70612afee017e3d51faa844
Mike <mikej.miller440@gmail.com>
2022-07-22 20:40:22 -0500

    incorporated react router dom

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import 'antd/dist/antd.min.css';
 import { Layout } from 'antd';
 import Navbar from './components/Navbar';
+import Events from './pages/Events';
+import EventInfo from './pages/EventInfo';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Navbar />
+        <Routes>
+          {/* <Route path="/" element={<Events></Events>}></Route> */}
+          <Route path="events" element={<Events />} />
+          <Route path="events/:eventId" element={<EventInfo />} />
+        </Routes>
       </Layout>
     </div>
   );
