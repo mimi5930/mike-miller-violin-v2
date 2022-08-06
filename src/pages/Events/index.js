c551b86672f2a299a6f04781d218e94eaf982466
mimi5930 <mikej.miller440@gmail.com>
2022-08-05 23:00:46 -0500

    changed event styling

--- src/pages/Events/index.js
+++ src/pages/Events/index.js
@@ -3,13 +3,32 @@ import Event from '../../components/Event';
 import { format } from 'date-fns';
 import { Card, Skeleton } from 'antd';
 import { sortDates } from '../../utils/helpers';
+import { useMediaQuery } from 'react-responsive';
 
 // redux imports
 import { useSelector, useDispatch } from 'react-redux';
 import { setEvents, completeLoading } from './eventsSlice';
 
+const styles = {
+  skeletonContainer: {
+    minHeight: 'calc(100vh - 100px)',
+    backgroundColor: '#ffecd1',
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  skeletonCard: { width: '72vw', height: '450px', margin: 10 },
+  skeleton: { padding: '50px' },
+  page: {
+    backgroundColor: '#ffecd1',
+    display: 'flex',
+    flexDirection: 'column'
+  }
+};
+
 export default function Events() {
   const dispatch = useDispatch();
+
+  const smallScreen = useMediaQuery({ query: '(max-width: 1150px)' });
 
   const eventsList = useSelector(state => state.events.value);
   const eventsLoading = useSelector(state => state.events.isLoading);
@@ -32,23 +51,27 @@ export default function Events() {
 
   if (eventsLoading)
     return (
-      <div style={{ marginRight: 'auto', marginLeft: 'auto' }}>
-        <Card style={{ width: '72vw', height: '450px', margin: 10 }}>
-          <Skeleton active style={{ padding: '50px' }} />
+      <div style={styles.skeletonContainer}>
+        <Card style={styles.skeletonCard}>
+          <Skeleton active style={styles.skeleton} />
         </Card>
       </div>
     );
 
-  return eventsList.map(event => {
-    return (
-      <div key={event.id} style={{ marginRight: 'auto', marginLeft: 'auto' }}>
-        <Event
-          id={event.id}
-          title={event.summary}
-          time={format(new Date(event.start.dateTime), 'EEEE MMM d, p')}
-          location={event.location}
-        />
-      </div>
-    );
-  });
+  return (
+    <div style={styles.page}>
+      {eventsList.map(event => {
+        return (
+          <Event
+            key={event.id}
+            id={event.id}
+            title={event.summary}
+            time={format(new Date(event.start.dateTime), 'EEEE MMM d, p')}
+            location={event.location}
+          />
+        );
+      })}
+      ;
+    </div>
+  );
 }
