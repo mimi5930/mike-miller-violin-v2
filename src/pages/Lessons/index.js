fcc799bb7e80db3130aff3987b580ef7a8b9c699
Mike <mikej.miller440@gmail.com>
2022-08-16 19:57:40 -0500

    began work on lessons page

--- src/pages/Lessons/index.js
+++ src/pages/Lessons/index.js
@@ -1,9 +1,28 @@
+import { Card, Divider } from 'antd';
 import React from 'react';
 
 export default function Lessons() {
   return (
-    <div>
-      <div>Lessons</div>
+    <div
+      style={{
+        minHeight: 'calc(100vh - 100px)',
+        backgroundColor: 'var(--background-color)'
+      }}
+    >
+      <h1
+        style={{
+          textAlign: 'center',
+          fontSize: '80px',
+          color: 'var(--title-color)',
+          marginBottom: 0
+        }}
+      >
+        Lessons
+      </h1>
+      <Divider></Divider>
+      <div>
+        <Card></Card>
+      </div>
     </div>
   );
 }
