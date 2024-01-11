cbec37d584505ef54791f6a209eea4b460bb6f5c
Mike <mikej.miller440@gmail.com>
2024-01-11 15:24:02 -0600

    added prettier config file

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid'
+};
+
+module.exports = config;
